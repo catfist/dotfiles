e5d8e501ef7c63d73777df1d15c1263b7358d546
catfist <catfist.m@gmail.com>
2015-09-06 21:21:59 +0900

    Add requirements

---
+++ requirements/without_manager.sh
@@ -0,0 +1,26 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: without_manager.sh
+# 
+#         USAGE: ./without_manager.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2015/07/29 20:59
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+
+
+pip install git+https://github.com/r-darwish/pushbullet-cli.git
+
