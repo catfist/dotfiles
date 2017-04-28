9eda62fe385170c8097f33a071fd198cf0671f0c
catfist <catfist.m@gmail.com>
2017-04-29 05:24:18 +0900

    slimize

--- .bash/conf/variable-init.bash
+++ .bash/conf/variable-init.bash
@@ -13,11 +13,11 @@ export GUIEDITOR="FoldingText"
 # homebrew
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 # gisty
-export GISTY_DIR=$(ghq root)/github.com/gisty
-export GISTY_SSL_VERIFY="NONE"
+# export GISTY_DIR=$(ghq root)/github.com/gisty
+# export GISTY_SSL_VERIFY="NONE"
 # path
 export bindir=~/bin
-export gr=$(ghq root)
+# export gr=$(ghq root)
 export alw=~/Dropbox/setting/Alfred.alfredpreferences/workflows
 export KOBITODIR=~/Dropbox/articles/kobito
 # sensitive
